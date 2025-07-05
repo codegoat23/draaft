5819eb4d1c93d1202e4314e8a72e788e21bfc882
codegoat23 <codesmrt@proton.me>
2025-07-04 17:31:34 -0700

    fix: load images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['vlmdgeqahhdpdrswaqsh.supabase.co'],
+  },
 };
 
 export default nextConfig;
